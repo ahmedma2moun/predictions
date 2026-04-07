959dbaff9fb0a01b762ab1e2f58f65b65b5aa11c
Ahmed Maamoun <ahmed.maamoun@tpay.me>
2026-04-07 19:27:15 +0200

    fixing issue

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "media.api-sports.io" },
-      { protocol: "https", hostname: "media-4.api-sports.io" },
+      { protocol: "https", hostname: "*.api-sports.io" },
     ],
   },
 };
