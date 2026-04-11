21097e3c20e68b263720dfefb0407ccfbb2d6ef6
Ahmed Maamoun <ahmed.maamoun@tpay.me>
2026-04-11 14:56:49 +0200

    fixing

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     staleTimes: { dynamic: 0 },
-    instrumentationHook: true,
   },
   images: {
     remotePatterns: [
